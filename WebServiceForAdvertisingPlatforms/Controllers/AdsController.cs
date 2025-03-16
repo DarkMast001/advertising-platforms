156ca322021c87cd184733db01ffb8f7cd1cac77
Dmitry <demaster.easy@gmail.com>
2025-03-16 22:12:15 +0300

    feat: web service has been created

---
+++ WebServiceForAdvertisingPlatforms/Controllers/AdsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebServiceForAdvertisingPlatforms.Service;
+
+namespace WebServiceForAdvertisingPlatforms.Controllers
+{
+    public class FileUploadRequest
+    {
+        public string FilePath { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdsController : ControllerBase
+    {
+        private readonly RegionTreeService _regionTreeService;
+
+        public AdsController(RegionTreeService regionTreeService)
+        {
+            _regionTreeService = regionTreeService;
+        }
+
+        // Загрузка данных из файла
+        [HttpPost("upload")]
+        public IActionResult UploadFile([FromBody] string filePath)
+        {
+            try
+            {
+                _regionTreeService.LoadDataFromFile(filePath);
+                return Ok($"Данные успешно загружены.");
+            }
+            catch (FileNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // Поиск рекламных площадок по региону
+        [HttpGet("search")]
+        public IActionResult SearchAds([FromQuery] string region)
+        {
+            var ads = _regionTreeService.GetAdsByRegion(region);
+            return Ok(ads);
+        }
+    }
+}
---
+++ WebServiceForAdvertisingPlatforms/Service/RegionTreeService.cs
@@ -0,0 +1,19 @@
+﻿using RegionTreeLib;
+
+namespace WebServiceForAdvertisingPlatforms.Service
+{
+    public class RegionTreeService
+    {
+        private RegionTree _regionTree;
+
+        public void LoadDataFromFile(string filePath)
+        {
+            _regionTree = new RegionTree(filePath);
+        }
+
+        public List<string> GetAdsByRegion(string region)
+        {
+            return _regionTree.findNote(region) ?? new List<string>();
+        }
+    }
+}
