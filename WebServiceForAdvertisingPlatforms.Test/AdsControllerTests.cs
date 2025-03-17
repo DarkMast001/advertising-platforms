d6af1aeec114e99128e768979e7fea8a2dafb544
Dmitry <demaster.easy@gmail.com>
2025-03-18 02:31:48 +0300

    controllers and services are covered by tests

---
+++ WebServiceForAdvertisingPlatforms.Test/AdsControllerTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServiceForAdvertisingPlatforms.Controllers;
+using WebServiceForAdvertisingPlatforms.Service;
+
+namespace WebServiceForAdvertisingPlatforms.Test
+{
+    public class AdsControllerTests
+    {
+        [Fact]
+        public void SearchAdsWithCorrectData()
+        {
+            RegionTreeService regionTreeService = new RegionTreeService();
+            regionTreeService.LoadDataFromFile("D://regions.txt");
+
+            AdsController controller = new AdsController(regionTreeService);
+
+            var result = controller.SearchAds("/ru/svrd");
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var ads = Assert.IsType<List<string>>(okResult.Value);
+            Assert.Contains("Яндекс.Директ", ads);
+            Assert.Contains("Крутая реклама", ads);
+        }
+
+        [Fact]
+        public void SearchAdsForNonExistentRegion()
+        {
+            RegionTreeService regionTreeService = new RegionTreeService();
+            regionTreeService.LoadDataFromFile("D://regions.txt");
+
+            AdsController controller = new AdsController(regionTreeService);
+
+            var result = controller.SearchAds("/eu");
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var ads = Assert.IsType<List<string>>(okResult.Value);
+            Assert.Empty(ads);
+        }
+
+        [Fact]
+        public void UploadFileWhenFileExists()
+        {
+            RegionTreeService regionTreeService = new RegionTreeService();
+            AdsController controller = new AdsController(regionTreeService);
+
+            var result = controller.UploadFile("D://regions.txt");
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Данные успешно загружены.", okResult.Value);
+        }
+
+        [Fact]
+        public void UploadFileWhenFileDoesNotExist()
+        {
+            RegionTreeService regionTreeService = new RegionTreeService();
+            AdsController controller = new AdsController(regionTreeService);
+
+            var result = controller.UploadFile("D://zxcccc.txt");
+
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal("File is not exist.", notFoundResult.Value);
+        }
+    }
+}
---
+++ WebServiceForAdvertisingPlatforms.Test/RegionTreeServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using WebServiceForAdvertisingPlatforms.Service;
+
+namespace WebServiceForAdvertisingPlatforms.Test
+{
+    public class RegionTreeServiceTests
+    {
+        [Fact]
+        public void GetAdsByRegion_ReturnsCorrectAds()
+        {
+            RegionTreeService service = new RegionTreeService();
+            service.LoadDataFromFile("D://regions.txt");
+
+            var result = service.GetAdsByRegion("/ru");
+
+            Assert.NotEmpty(result);
+            Assert.Contains("Яндекс.Директ", result);
+        }
+
+        [Fact]
+        public void WrongFilePath()
+        {
+            RegionTreeService service = new RegionTreeService();
+
+            Assert.Throws<FileNotFoundException>(() => service.LoadDataFromFile("zxc"));
+            Assert.Throws<FileNotFoundException>(() => service.LoadDataFromFile("./zxc"));
+            Assert.Throws<ArgumentException>(() => service.LoadDataFromFile("D://regions.json"));
+        }
+    }
+}
